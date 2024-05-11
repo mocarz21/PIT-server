008cc8c5e5d14049bc5541701b854a4ae17b45fc
Adam <adammocarski22@gmail.com>
2024-05-11 13:31:20 +0200

    Add routes references

--- routes.js
+++ routes.js
@@ -3,10 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const CertificatesControllers = require('./controllers/certificates')
-router.get('/certificates/', CertificatesControllers.showAll )
+router.get('/certificates', CertificatesControllers.showAll )
 router.post('/certificates', CertificatesControllers.save )
 router.post('/certificates/:id/remove', CertificatesControllers.remove )
 
+const  ReferencesControllers = require('./controllers/references')
+router.get('/references',)
+router.post('/references',)
+router.post('/references/:id/remove',)
 
 
 
